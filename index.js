646bd8f9cca43549d510f7bcfb6145046334c07a
Evanskonto <72521280+Evanskonto@users.noreply.github.com>
2023-01-26 12:37:21 +0200

    initial commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 
 const cors = require("cors");
@@ -30,7 +29,6 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/', express.static(path.join(__dirname, 'views')))
