a80ac2594d63ad5ae52eb17ceb0719c318f311b5
Evanskonto <72521280+Evanskonto@users.noreply.github.com>
2023-01-26 17:29:02 +0200

    initial change

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
 
@@ -26,7 +25,6 @@ mongoose
 
 
 app.use(express.json());
-app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
